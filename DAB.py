eaaf762f41150a1b70435ba18b7d64f1ad54daf1
domenicoblanco <doddo97@me.com>
2024-03-27 17:01:15 +0100

    Moved to aiohttp, removed whitespaces and improved init

--- DAB.py
+++ DAB.py
@@ -1,174 +1,187 @@
-from requests import Session
-from json import load, loads, dump, dumps, JSONDecodeError
+import aiohttp
+from json import dumps, loads, JSONDecodeError
+from asyncio import get_event_loop, create_task, new_event_loop
 from datetime import datetime
-
 import const
 import logging
 
 class DAB():
-    def __init__(self, email: str, psw: str, should_save_token: bool = True) -> None:
+    def __init__(self, email: str, psw: str, session: aiohttp.ClientSession|None = None, should_save_token: bool = True) -> None:
         self.token = None
         self.email = None
         self.psw = None
         self.save_token = should_save_token
-        self.session = Session()
+        self.session = session
+        self.should_close_session = session is None
         self.set_credentials(email, psw)
-        self.__check_token()
-    
+        self.__check_token__()
+
 
     def __del__(self) -> None:
-        self.session.close()
+        if self.session and self.should_close_session:
+            try:
+                loop = get_event_loop()
+                create_task(self.session.close())
+            except RuntimeError:
+                loop = new_event_loop()
+                loop.run_until_complete(self.session.close())
 
 
     def set_credentials(self, email: str|None = None, psw: str|None = None) -> None:
         self.email = email
         self.psw = psw
 
-    
-    def __check_token(self) -> None:
-        if self.save_token:
-            with open('.dab_token', 'w+') as j:
-                try:
-                    token_data = load(j)
 
+    def __check_token__(self) -> None:
+        if self.save_token:
+            with open('.dab_token', 'w+') as saved_data:
+                try:
+                    token_data = loads(saved_data)
                     if 'expire' in token_data and datetime.now() < token_data['expire']:
-                        logging.warning('Using stored token')
+                        logging.info('Using stored token')
                         self.token = token_data['token']
-                except JSONDecodeError:
+                        return
+                except (JSONDecodeError, TypeError) as e:
+                    logging.error('__check_token__', e)
                     return
 
 
-    def __authenticate(self) -> bool:
+    async def __authenticate__(self) -> bool:
         if self.email is None or self.psw is None:
-            logging.warning("Credentials not set!")
+            logging.warning('Credentials not set!')
             return False
 
-        auth_header = {
-            'host': 'dconnect.dabpumps.com',
-            'content-type': 'application/json',
-            'connection': 'keep-alive',
-            'accept': 'application/json, text/plain, */*',
-            'user-agent': 'DabAppFreemium/1 CFNetwork/1406.0.4 Darwin/22.4.0',
-            'content-length': '48',
-            'accept-language': 'en-GB,en;q=0.9',
-            'accept-encoding': 'gzip, deflate, br'
+        if self.session is None:
+            self.session = aiohttp.ClientSession(headers=const.DEFAULT_HEADER)
+
+        auth_payload = {
+            'email': self.email,
+            'password': self.psw
         }
 
-        auth_payload = dumps({
-            "email": self.email,
-            "password": self.psw
-        })
+        async with self.session.post(f'{const.BASE_URL}{const.AUTH}', json=auth_payload) as auth_response:
+            current_time = datetime.now().timestamp()
 
-        auth_response = self.session.post(f'{const.BASE_URL}{const.AUTH}', headers=auth_header, data=auth_payload)
-        current_time = datetime.now().timestamp()
+            try:
+                auth_response = await auth_response.json()
+            except Exception as e:
+                logging.error('Something went wrong during authentication', e)
+                return False
 
-        try:
-            auth_response = auth_response.json()
-        except Exception as e:
-            logging.error('Something went wrong during authentication', e)
-        
-        if 'code' in auth_response:
-            logging.warning(auth_response['code'], auth_response['msg'])
-            return False
-        else:
-            logging.info('Login succeeded')
-            self.token = auth_response['access_token']
+            if 'code' in auth_response:
+                logging.warning(auth_response['code'], auth_response['msg'])
+                return False
+            else:
+                logging.info('Login succeeded')
+                self.token = auth_response['access_token']
 
-            if self.save_token:
-                with open('.dab_token', 'w') as j:
-                    dump({
-                        'token': self.token,
-                        'expire': current_time + float(auth_response['expires_in'])
-                    }, j)
+                if self.save_token:
+                    with open('.dab_token', 'w') as save_data:
+                        save_data.write(dumps({
+                            'token': self.token,
+                            'expire': current_time + float(auth_response['expires_in'])
+                        }))
 
-            return True    
-    
+                return True    
 
-    def discover_installation_by_id(self, id: str, name: str) -> list[str]:
+
+    async def discover_installation_by_id(self, id: str, name: str) -> list[str]:
         installation_pumps = []
         header = const.DEFAULT_HEADER.copy()
         header['authorization'] += self.token
 
-        installation_data = self.session.get(f'{const.BASE_URL}{const.INSTALLATION}/{id}', headers=header).json()
+        async with self.session.get(f'{const.BASE_URL}{const.INSTALLATION}/{id}', headers=header) as installation_response:
+            installation_data = await installation_response.json()
 
-        if installation_data['res'] != 'OK':
-            logging.warning(f'Something went wrong with installation {name} ({id}) ', installation_data)
-        
-        dum_list = loads(installation_data['data'])['dumlist']
-        for dum in dum_list:
-            installation_pumps.append(dum['serial'])
+            if installation_data['res'] != 'OK':
+                logging.warning(f'Something went wrong with installation {name} ({id}) ', installation_data)
+
+            dum_list = loads(installation_data['data'])['dumlist']
+            for dum in dum_list:
+                installation_pumps.append(dum['serial'])
     
         return installation_pumps
 
 
-    def discover_installations(self) -> list[dict[str, any]]:
-        if self.token is None and not self.__authenticate():
+    async def discover_installations(self) -> list[dict[str, any]]:
+        if self.token is None and not await self.__authenticate__():
             return []
 
         installations = []
         request_header = const.DEFAULT_HEADER.copy()
         request_header['authorization'] += self.token
 
-        discovery_res = self.session.get(f'{const.BASE_URL}{const.INSTALLATION_LIST}', headers=request_header).json()
+        async with self.session.get(f'{const.BASE_URL}{const.INSTALLATION_LIST}', headers=request_header) as discovery_response:
+            discovery_res = await discovery_response.json()
 
-        if discovery_res['res'] != 'OK':
-            logging.warning('Something went wrong: ', discovery_res)
-            return []
-        
-        for installation in discovery_res['rows']:
-            installation_details = {
-                'id': installation['installation_id'],
-                'name': installation['name'],
-                'description': installation['description'],
-                'company': installation['company'],
-                'status': installation['status'],
-                'pumps': self.discover_installation_by_id(installation['installation_id'], installation['name'])
-            }
-            
-            installations.insert(0, installation_details)
+            if discovery_res['res'] != 'OK':
+                logging.warning('Something went wrong during discovery: ', discovery_res)
+                return []
+
+            for installation in discovery_res['rows']:
+                pumps_data = {}
+
+                for pump_id in await self.discover_installation_by_id(installation['installation_id'], installation['name']):
+                    pumps_data[pump_id] = await self.request_pump_data(pump_id)
+
+                installation_details = {
+                    'id': installation['installation_id'],
+                    'name': installation['name'],
+                    'description': installation['description'],
+                    'company': installation['company'] if type(installation['company']) == str else ', '.join(installation['company']),
+                    'status': installation['status'],
+                    'pumps': pumps_data
+                }
+
+                installations.insert(0, installation_details)
 
         return installations
-    
 
-    def request_pump_data(self, pump_serial: str|None = None) -> list[dict[str, any]]:
+
+    async def request_pump_data(self, pump_serial: str|None = None) -> list[dict[str, any]]:
         header = const.DEFAULT_HEADER.copy()
         header['authorization'] += self.token
 
-        pump_data = self.session.get(f'{const.BASE_URL}{const.DUMSTATE}{pump_serial}', headers=header).json()
+        async with self.session.get(f'{const.BASE_URL}{const.DUMSTATE}{pump_serial}', headers=header) as pump_data_response:
+            pump_data = await pump_data_response.json()
 
-        if pump_data['res'] == 'ERROR':
-            logging.error(f'{pump_data["code"]}: {pump_data["msg"]}')
+            if pump_data['res'] == 'ERROR':
+                logging.error(f'{pump_data['code']}: {pump_data['msg']}')
 
         return loads(pump_data['status'])
-    
 
-    def request_installation_data(self, installation_id: str|None = None) -> list[dict[str,any]]:
+
+    async def request_installation_data(self, installation_id: str|None = None) -> list[dict[str,any]]:
         pumps = []
         pumps_data = []
+
         if installation_id is None:
-            installations = self.discover_installations()
+            installations = await self.discover_installations()
 
             for installation in installations:
                 if 'pumps' in installation:
                     pumps += installation['pumps']
         else:
-            pumps = self.discover_installation_by_id(installation_id, 'provided')
+            pumps = await self.discover_installation_by_id(installation_id, 'provided')
         
         for pump_id in pumps:
-            pumps_data.append(self.request_pump_data(pump_id))
-        
+            pumps_data.insert(0, await self.request_pump_data(pump_id))
+
         return pumps_data
-    
-    def enable_power_shower(self, pump_id: str) -> bool:
-        endpoint = f'{const.BASE_URL}dum/{pump_id}{const.ENABLE_POWER_SHOWER}'
+
+
+    async def handle_power_shower(self, pump_id: str, op: str) -> bool:
+        endpoint = f'{const.BASE_URL}dum/{pump_id}{const.POWER_SHOWER}{op}'
         
         request_header = const.DEFAULT_HEADER.copy()
         request_header['authorization'] += self.token
 
-        power_shower_res = self.session.post(endpoint, headers=request_header).json()
+        async with self.session.post(endpoint, headers=request_header) as power_shower_response:
+            power_shower_res = await power_shower_response.json()
 
-        if 'res' in power_shower_res and power_shower_res['res'] == 'OK':
+        if power_shower_res.get('res') == 'OK':
             return True
         else:
-            logging.error(f'There was an error while enabling power shower\n{power_shower_res["code"]}: {power_shower_res["msg"]}')
+            logging.error(f'There was an error while setting power shower {op}\n{power_shower_res['code']}: {power_shower_res['msg']}')
             return False
+
